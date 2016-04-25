e383b171c91ed3acbfa838e0aa05d51ad6e239f4
Paul Raff <paul@myraff.com>
2016-04-24 17:18:30 -0700

    First submission, and working through bug.

---
+++ Algorithms/Algorithms/Sorting.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+
+namespace Raff.Algorithms
+{
+    public class Sorting
+    {
+        // Merge sort - specific to integers
+        public static int[] MergeSort(int[] inputList)
+        {
+            var inputListLength = inputList.Length;
+            // Edge cases: 0 and 1 
+            if (inputListLength < 2)
+                return inputList;
+
+            // Since length will be at least 2, then this will be at least 1
+            var midpoint = inputListLength / 2;
+
+            var leftArray  = inputList.Take(midpoint).ToArray();
+            var rightArray = inputList.Skip(midpoint).ToArray();
+
+            var leftArraySorted  = MergeSort(leftArray);
+            var rightArraySorted = MergeSort(rightArray);
+
+            return MergeSortMerge(leftArraySorted, rightArraySorted);
+        }
+
+        // Merge sort - the merge part
+        // We assume here that the input arrays are sorted.
+        // Undefined behavior if they are not!
+        private static int[] MergeSortMerge(int[] leftArray, int[] rightArray)
+        {
+            // Edge case - either input is empty
+            if (leftArray.Length == 0)
+                return rightArray;
+            if (rightArray.Length == 0)
+                return leftArray;
+
+            var result = new int[leftArray.Length + rightArray.Length];
+
+            var leftCounter = 0;
+            var rightCounter = 0;
+            var resultCounter = 0;
+
+            while (leftCounter < leftArray.Length && rightCounter < rightArray.Length)
+            {
+                var leftValue = leftArray[leftCounter];
+                var rightValue = rightArray[rightCounter];
+
+                if (leftValue < rightValue)
+                {
+                    result[resultCounter++] = leftValue;
+                    leftCounter++;
+                }
+                else if (leftValue > rightValue)
+                {
+                    result[resultCounter++] = rightValue;
+                    rightCounter++;
+                }
+                else
+                {
+                    result[resultCounter++] = leftValue;
+                    result[resultCounter++] = rightValue;
+                    leftCounter++;
+                    rightCounter++;
+                }
+            }
+
+            // Out of the while loop, we've hit the endpoint of one of our arrays
+            if (leftCounter == leftArray.Length)
+            {
+                while (rightCounter < rightArray.Length)
+                {
+                    result[resultCounter] = rightArray[rightCounter];
+                    rightCounter++;
+                }
+            }
+            else
+            {
+                while (leftCounter < leftArray.Length)
+                {
+                    result[resultCounter] = leftArray[leftCounter];
+                    leftCounter++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algorithms/AlgorithmsTest/SortingTest.cs
@@ -0,0 +1,65 @@
+﻿using Raff.Algorithms;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsTest
+{
+    [TestClass]
+    public class SortingTest
+    {
+        [TestMethod]
+        public void merge_sort_empty_input_empty_output()
+        {
+            int[] input = { };
+
+            int[] actual = Sorting.MergeSort(input);
+            int[] expected = { };
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void merge_sort_one_element_input_same_output()
+        {
+            int[] input = { 4 };
+
+            int[] actual = Sorting.MergeSort(input);
+            int[] expected = { 4 };
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void merge_sort_two_elements_sorted_same_output()
+        {
+            int[] input = { 4, 8 };
+
+            int[] actual = Sorting.MergeSort(input);
+            int[] expected = { 4, 8 };
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void merge_sort_two_elements_sorts_correctly()
+        {
+            int[] input = { 8, 4 };
+
+            int[] actual = Sorting.MergeSort(input);
+            int[] expected = { 4, 8 };
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void merge_sort_ten_element_list()
+        {
+            int[] input = { 2, 1, 5, 6, 7, 9, 2, 5, 7, 1 };
+
+            int[] actual = Sorting.MergeSort(input);
+            int[] expected = { 1, 1, 2, 2, 5, 5, 6, 7, 7, 9 };
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+    }
+}
