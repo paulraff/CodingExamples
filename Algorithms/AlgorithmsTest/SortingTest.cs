a50ac2acd8a2b24cd04b7321833440c03658cd5b
Paul Raff <paul@myraff.com>
2016-04-24 17:38:00 -0700

    Added generic implementation just like that.

--- Algorithms/AlgorithmsTest/SortingTest.cs
+++ Algorithms/AlgorithmsTest/SortingTest.cs
@@ -19,12 +19,34 @@ namespace AlgorithmsTest
         }
 
         [TestMethod]
+        public void merge_sort_generic_empty_input_empty_output()
+        {
+            string[] input = { };
+
+            string[] actual = Sorting.MergeSort(input);
+            string[] expected = { };
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
         public void merge_sort_one_element_input_same_output()
         {
             int[] input = { 4 };
 
             int[] actual = Sorting.MergeSort(input);
             int[] expected = { 4 };
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void merge_sort_generic_one_element_input_same_output()
+        {
+            string[] input = { "fa" };
+
+            string[] actual = Sorting.MergeSort(input);
+            string[] expected = { "fa" };
 
             actual.ShouldBeEquivalentTo(expected);
         }
@@ -41,12 +63,34 @@ namespace AlgorithmsTest
         }
 
         [TestMethod]
+        public void merge_sort_generic_two_elements_sorted_same_output()
+        {
+            string[] input = { "fa", "la" };
+
+            string[] actual = Sorting.MergeSort(input);
+            string[] expected = { "fa", "la" };
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
         public void merge_sort_two_elements_sorts_correctly()
         {
             int[] input = { 8, 4 };
 
             int[] actual = Sorting.MergeSort(input);
             int[] expected = { 4, 8 };
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void merge_sort_generic_two_elements_sorts_correctly()
+        {
+            string[] input = { "la", "fa" };
+
+            string[] actual = Sorting.MergeSort(input);
+            string[] expected = { "fa", "la" };
 
             actual.ShouldBeEquivalentTo(expected);
         }
@@ -63,12 +107,34 @@ namespace AlgorithmsTest
         }
 
         [TestMethod]
+        public void merge_sort_generic_three_element_list()
+        {
+            string[] input = { "ba", "fa", "la"};
+
+            string[] actual = Sorting.MergeSort(input);
+            string[] expected = { "ba", "fa", "la" };
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
         public void merge_sort_ten_element_list()
         {
             int[] input = { 9, 8, 7, 6, 5, 5, 4, 3, 2, 1 };
 
             int[] actual = Sorting.MergeSort(input);
             int[] expected = { 1, 2, 3, 4, 5, 5, 6, 7, 8, 9 };
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void merge_sort_generic_ten_element_list()
+        {
+            string[] input = { "do", "re", "mi", "fa", "so", "la", "ti", "do", "boo", "yah" };
+
+            string[] actual = Sorting.MergeSort(input);
+            string[] expected = { "do", "do", "fa", "la", "mi", "re", "so", "ti", "boo", "yah"};
 
             actual.ShouldBeEquivalentTo(expected);
         }
